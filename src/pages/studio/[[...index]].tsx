7a65aadb4908c65178b0df6f0494174399dfcff9
Sovattha Sok <github@soks.fr>
2024-10-29 23:23:44 +0100

    Fix a type

--- src/pages/studio/[[...index]].tsx
+++ src/pages/studio/[[...index]].tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import { NextStudio } from 'next-sanity/studio'
-import { metadata } from 'next-sanity/studio/metadata'
+import { metadata, NextStudio } from 'next-sanity/studio'
 import config from 'sanity.config'
 
 export default function StudioPage() {
